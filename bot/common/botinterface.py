90a9f58d22c6a995e6b370b69934a6e04dd73b45
Anson <git@anson-cheung.com>
2019-10-19 00:36:07 +0100

    Add turn angle

--- bot/common/botinterface.py
+++ bot/common/botinterface.py
@@ -7,6 +7,15 @@ class BotInterface:
         self.game_server = game_server
         self.game_server.sendMessage(ServerMessageTypes.CREATETANK, {'Name': name})
 
+    @staticmethod
+    def get_coords(payload):
+        """
+        Helper function that makes a tuple of coordinates from a payload
+        :param payload: Dictionary payload
+        :return: The tuple of coordinates
+        """
+        return payload["X"], payload["Y"]
+
     # TODO: Method that returns a tuple of angle and distance to an object
     def distance_angle_object(self, coordinate: tuple, heading: int):
         pass
--- bot/shootybot.py
+++ bot/shootybot.py
@@ -8,13 +8,19 @@ class ShootyBot(BotInterface):
         super().__init__(game_server, name)
         self.last_message = None
 
+    # TODO: Method that determines whether the tank should turn left or right on a target. Should return either
+    #   ServerMessageTypes left or right
+    def determine_turn_angle(self, angle: int):
+        pass
+
     def rx(self):
         self.last_message = self.game_server.readMessage()
         # self.messages.append(self.game_server.readMessage())
 
     def action(self):
-        self.game_server.sendMessage(ServerMessageTypes.TOGGLELEFT)
         if not self.last_message["messageType"] == 18:
             return
+        distance, angle = self.distance_angle_object(self.get_coords(self.last_message), self.last_message["Heading"])
+
         self.game_server.sendMessage(ServerMessageTypes.FIRE)
 
