bcaeb644ecfab71fb217aef2e5e22e9161e5cdcc
Anson <git@anson-cheung.com>
2019-10-19 01:23:10 +0100

    Make skeleton for distance to object

--- bot/common/botinterface.py
+++ bot/common/botinterface.py
@@ -1,3 +1,5 @@
+import math
+
 from bot.common.servercomms import ServerComms
 from bot.common.servermessagetypes import ServerMessageTypes
 
@@ -6,6 +8,7 @@ class BotInterface:
     def __init__(self, game_server: ServerComms, name: str):
         self.game_server = game_server
         self.game_server.sendMessage(ServerMessageTypes.CREATETANK, {'Name': name})
+        # TODO: set xpos and ypos
 
     @staticmethod
     def get_coords(payload):
@@ -19,6 +22,12 @@ class BotInterface:
     # TODO: Method that returns a tuple of angle and distance to an object
     def distance_angle_object(self, coordinate: tuple, heading: int):
         pass
+
+    def distance_to_object(self, coordinate: tuple):
+        x, y = coordinate
+        delta_x = self.xpos - x
+        delta_y = self.ypos - y
+        return ((math.atan2(delta_y, delta_x)*(180/math.pi)) - 360) % 360
 
     def rx(self):
         raise NotImplementedError
