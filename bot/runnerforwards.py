ae532f3cbe99280431c75833ac737eb5b5a53908
Anson <git@anson-cheung.com>
2019-10-19 10:54:50 +0100

    Move controller to separate directory

--- bot/runnerforwards.py
+++
@@ -1,8 +0,0 @@
-from bot.forwards import ForwardsBot
-from bot.common.servercomms import ServerComms
-
-gs = ServerComms('127.0.0.1', 8052)
-bot = ForwardsBot(gs, "TeamA:Bot1")
-while True:
-    bot.rx()
-    bot.action()
--- bot/shootybotrunner.py
+++
@@ -1,8 +0,0 @@
-from bot.shootybot import ShootyBot
-from bot.common.servercomms import ServerComms
-
-gs = ServerComms('127.0.0.1', 8052)
-bot = ShootyBot(gs, "TeamA:Bot1")
-while True:
-    bot.rx()
-    bot.action()
