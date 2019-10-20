68611b5004e52025d71e5bc0d3acf8e3f44affbe
Luna <github@luna.allan.scot>
2019-10-20 12:13:55 +0100

    Turret spin strategy shooting

--- strategies/turret/turretspin.py
+++ strategies/turret/turretspin.py
@@ -1,5 +1,3 @@
-import time
-
 from common.servermessagetypes import ServerMessageTypes
 from strategies import Strategy
 
@@ -10,7 +8,31 @@ class TurretSpin(Strategy):
         self.spin = False
         self.start = True
 
+    def try_shoot(self, bot):
+        if bot.target and bot.can_fire():
+            position = bot.tracker.positions[bot.target]
+            if not position.is_stale():
+                bot.game_server.sendMessage(ServerMessageTypes.STOPTURRET)
+                self.spin = False
+                # todo targetting
+
+                firing_angle = bot.angle_to_object(position.pos)
+                bot.turn_turret_to_heading(firing_angle)
+                dist = bot.distance_to_object(position.pos)
+
+                angle_deviation = abs(bot.turret_heading - firing_angle)
+
+                if dist < 30 and angle_deviation < (100 / dist):
+                    bot.fire()
+
+                return True
+
+        return False
+
     def action(self, bot):
+        trying_to_shoot = self.try_shoot(bot)
+        self.want_spin = not trying_to_shoot
+
         if self.start:
             bot.game_server.sendMessage(ServerMessageTypes.STOPTURRET)
             self.start = False
@@ -20,17 +42,3 @@ class TurretSpin(Strategy):
         if not self.want_spin and self.spin:
             bot.game_server.sendMessage(ServerMessageTypes.STOPTURRET)
             self.spin = False
-
-        if bot.target and bot.can_fire():
-            position = bot.tracker.positions[bot.target]
-            if not position.is_stale():
-                # todo targetting
-
-                firing_angle = bot.angle_to_object(position.pos)
-                bot.turn_turret_to_heading(firing_angle)
-                dist = bot.distance_to_object(position.pos)
-
-                angle_deviation = abs(bot.turret_heading - firing_angle)
-
-                if dist < 30 and angle_deviation < (100/dist):
-                    bot.fire()
