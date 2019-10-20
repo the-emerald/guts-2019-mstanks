69789fa6b9ef2c66b35cea9dace9a2bd287de47b
Anson <git@anson-cheung.com>
2019-10-20 06:38:25 +0100

    Add an if-in-range-then-grab-bot

--- strategies/greedy.py
+++ strategies/greedy.py
@@ -8,6 +8,14 @@ from strategies import Strategy
 class GreedyStrategy(Strategy):
     def __init__(self):
         self.standoff = 20
+        self.trigger = 30
+
+    def in_range(self, bot: Bot):
+        x, y = bot.target.pos
+        for key, value in bot.tracker.positions:
+            if bot.distance_to_object(value.pos) <= self.trigger:
+                return True
+        return False
 
     def action(self, bot: Bot):
         # Assume the controller gives us a target (box) that we need to keep an eye on
@@ -17,16 +25,22 @@ class GreedyStrategy(Strategy):
             return
         target_coords = bot.tracker.positions[target].pos
 
-        # Go to standoff distance
         moved = False
         while bot.distance_to_object(target_coords) >= self.standoff:
+            # Go to standoff distance
             target_distance = bot.distance_to_object(target_coords)
             target_angle = bot.angle_to_object(target_coords)
             bot.game_server.sendMessage(ServerMessageTypes.TURNTOHEADING, {"Amount": target_angle})
             bot.game_server.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE, {"Amount": target_distance})
             moved = True
             time.sleep(0.1)
+        if self.in_range(bot):
+            bot.game_server.sendMessage(ServerMessageTypes.TURNTOHEADING, {"Amount":
+                                        bot.angle_to_object(target_coords)})
+            bot.game_server.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE, {"Amount":
+                                        bot.distance_to_object(target_coords)})
         if moved:
+            # Orbit
             bot.game_server.sendMessage(ServerMessageTypes.STOPALL)
             target_coords = bot.tracker.positions[target].pos
             target_angle = (bot.angle_to_object(target_coords) + 80) % 360
