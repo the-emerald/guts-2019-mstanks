c2a523ae4cf53b742334c701ba2fa2c1ab3b6bb6
Luna <github@luna.allan.scot>
2019-10-20 06:48:04 +0100

    Add is_stale to ObjectStae

--- controller/tracker.py
+++ controller/tracker.py
@@ -1,10 +1,13 @@
 import logging
+import time
 from typing import Dict
 
 from common.servermessagetypes import ServerMessageTypes
 
 
 class ObjectState:
+    STALE_TIME = 5
+
     def __init__(self, pos, heading, turret_heading, type, id, name):
         self.pos = pos
         self.heading = heading
@@ -12,6 +15,10 @@ class ObjectState:
         self.type = type
         self.id = id
         self.name = name
+        self.time = time.time()
+
+    def is_stale(self):
+        return time.time() - self.time > ObjectState.STALE_TIME
 
 
 class Tracker:
@@ -32,4 +39,4 @@ class Tracker:
             id = message['Id']
             type = message['Type']
             self.positions[id] = ObjectState(pos, heading, turret_heading, type, id, name)
-            logging.info('Tracked %s at %s', name, pos)
+            logging.debug('Tracked %s at %s', name, pos)
