875fb7f9047c4d74b0a1b118f6e1fa4800bccd3b
Anson <git@anson-cheung.com>
2019-10-20 07:39:02 +0100

    Refactor turret spin

--- strategies/turretspin.py
+++ strategies/turretspin.py
@@ -1,21 +1,20 @@
 from common.servermessagetypes import ServerMessageTypes
 from strategies import Strategy
 
+
 class TurretSpin(Strategy):
     def __init__(self):
-        self.want_spin = 1
-        self.spin = 0
+        self.want_spin = True
+        self.spin = False
         self.start = True
 
     def action(self, bot):
-        if self.start == True:
+        if self.start:
             bot.game_server.sendMessage(ServerMessageTypes.STOPALL)
             self.start = False
-        if self.want_spin == 1:
-            if self.spin == 0:
-                bot.game_server.sendMessage(ServerMessageTypes.TOGGLETURRETRIGHT)
-                self.spin = 1
-        if self.want_spin == 0:
-            if self.spin == 1:
-                bot.game_server.sendMessage(ServerMessageTypes.STOPTURRET)
-                self.spin = 0
+        if self.want_spin and not self.spin:
+            bot.game_server.sendMessage(ServerMessageTypes.TOGGLETURRETRIGHT)
+            self.spin = True
+        if not self.want_spin and self.spin:
+            bot.game_server.sendMessage(ServerMessageTypes.STOPTURRET)
+            self.spin = False
